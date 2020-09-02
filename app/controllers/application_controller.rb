408ee4c74eb88b505e05f65c44658169e78177ad
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-02 12:26:17 +0900

    userモデルの単体テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'ryousuke' && password == '1637'
