ff6cba8d6258c06090803a82fd49e0b544422c35
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-03 14:50:54 +0900

    トップページから新規登録画面とログイン画面に遷移できるようにする。

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
-  resources :registrations, only: [:index, :new]
+  resources :registrations, only: [:new]
+  resources :sessions, only: [:new]
 end
