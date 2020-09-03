d1e006b02298c875de599b1e8fb539b7ae5f33d6
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-03 18:25:12 +0900

    usersテーブルに必要なカラムの追加

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,12 @@
 class RegistrationsController < ApplicationController
   def new
-    
   end
+
+  def create
+    if current_user.create(user_params)
+      redirect_to root_path
+    end
+   else
+    render :edit
+   end
 end
