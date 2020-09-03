f56fb3a4fd99cfde9347ca9367e37495e3722175
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-03 10:39:44 +0900

    トップページの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :basic_auth
+ # before_action :authenticate_user!
+  #
 
   private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'ryousuke' && password == '1637'
-    end
-  end
+  #
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == 'ryousuke' && password == '1637'
+  #   end
+  # end
 end
