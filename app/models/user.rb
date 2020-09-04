a5c16102df18e06839777ce93ae442c842052a9f
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-04 20:00:15 +0900

    不要なコメントの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :password, presence: true, length: { maximum: 6 }
-
+  
   with_options presence: true do
     validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
      validates :nickname
@@ -15,9 +13,4 @@ class User < ApplicationRecord
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birth_date
   end
-  # validates :nickname, presence: true
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :first_name_kana, presence: true
-  # validates :last_name_kana, presence: true
 end
