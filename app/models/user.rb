cd95f85c0a7de452c4f3243627e05a9d461d29d0
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-04 16:07:08 +0900

    バリデーションをwith_optionでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # validates :password, presence: true, length: { maximum: 6 }
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_date, presence: true
+
+  with_options presence: true do
+    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birth_date
+  end
+  # validates :nickname, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :last_name_kana, presence: true
 end
