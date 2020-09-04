12e083a0196c330653e8dd1d9ef38cddf489961e
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-04 14:53:40 +0900

    テストコードの編集と、gemの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
+  validates :birth_date, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe User, type: :model do
     it "nicknameが空では登録できないこと" do
       @user.nickname = nil
       @user.valid?
+      binding.pry
       expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
 
@@ -58,31 +59,31 @@ RSpec.describe User, type: :model do
     it "first_nameが空では登録できないこと" do
       @user.first_name = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Frist_name can't be blank")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it "last_nameが空では登録できないこと" do
       @user.last_name = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name can't be blank")
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it "first_name_kanaが空では登録できないこと" do
       @user.first_name_kana = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("First_name_kana can't be blank")
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
     end
 
     it "last_name_kanaが空では登録できないこと" do
       @user.last_name_kana = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name_kana can't be blank")
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
     end
 
     it "birth_dateが空では登録できないこと" do
       @user.birth_date = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Birth_date can't be blank")
+      expect(@user.errors.full_messages).to include("Birth date can't be blank")
     end
   end
 end
