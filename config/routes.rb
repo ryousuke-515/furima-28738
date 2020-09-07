ae65048d14c868b2bb8d2ce709e020de6b56eff4
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-07 11:02:25 +0900

    商品出品ページを表示するルーティングとコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
+  resources :items, only: [:new]
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new]
 end
