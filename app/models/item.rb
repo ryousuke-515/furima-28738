4c794c25073394a01da7f74f244911f5c18c004c
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-07 12:52:05 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to_active_hash :genre
   has_one :item_purchase
+
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charges_id
+    validates :area_id
+    validates :shiiing_days_id
+    validates :price
+  end
 end
