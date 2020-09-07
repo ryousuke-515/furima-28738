6345ac9113e3a9c29c5b25a4e7dbe92eddf4cbd3
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-07 11:10:57 +0900

    ミス

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new]
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new]
 end
