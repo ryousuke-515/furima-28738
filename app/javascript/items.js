aa3eb5bf6e7b7c6f6ef4e52a7d204e01caec32c9
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-08 19:19:39 +0900

    不要な記述の削除

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -2,15 +2,12 @@ function sale() {
   const price = document.getElementById("item-price")
   const fee = document.getElementById("add-tax-price")
   const profit = document.getElementById("profit")
-  console.log(price)
   price.addEventListener('input', function(){
   a = price.value
-  console.log(a)
   value = price.innerHTML
   commission = Math.floor(a * 0.1)
   fee.innerHTML = commission
   profit.innerHTML = a - commission
-  console.log(value)
   })
 }
 window.addEventListener('load', sale)
