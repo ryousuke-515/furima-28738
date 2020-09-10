c38e31229d587d2b2c9d52703ff458ae688a4517
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-10 10:24:09 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def edit 
+  def edit
   end
 
   private
