322cc227cdf968f50fea78af1da37a12dc13f59a
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-10 14:51:23 +0900

    編集できなかった場合の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -24,8 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    item.update(item_params)
+   if @item.update(item_params)
+    redirect_to item_path(@item.id)
+   else
+     render :edit
+   end 
+ 
   end
 
   private
