8e168b3c2dcbfa3dc838566cdf64c5eaa42c3fb0
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-11 17:30:28 +0900

    購入画面に遷移するルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
+  resources :orders, only: [:index, :create]
   resources :items
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new]
