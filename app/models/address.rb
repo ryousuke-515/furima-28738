10f50a9983680b92ca61cef4d35d123cbd06a672
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-11 19:16:43 +0900

    バリデーションの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,12 @@ class Address < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to :item_purchase
 
+
+  with_options presence: true do
+    validates :post_code
+    validates :area_id, numericality: { other_than: 0 }
+    validates :city
+    validates :address
+    validates :phone_number
+  end
 end
