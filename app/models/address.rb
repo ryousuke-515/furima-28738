b9e6342080720549d63bfb818e72d2cea38e1f67
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-15 10:47:48 +0900

    保存場所の指定

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,10 +5,20 @@ class Address < ApplicationRecord
 
 
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is sure to enter 7 digits including hyphens.' }
     validates :area_id, numericality: { other_than: 0 }
-    validates :city
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is sure to enter numerical values' }
   end
 end
+
+# with_options presence: true do
+#   validates :password, format: { with: /\A[a-z0-9]+\z/i, message: 'is invalid. Input half-width characters.' }
+#   validates :nickname
+#   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+#   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+#   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+#   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+#   validates :birth_date
+# end
