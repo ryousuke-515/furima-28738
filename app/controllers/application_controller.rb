30de1a01f7b84c994e5f90a687312cf80a22d89c
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-18 11:47:45 +0900

    環境変数

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'ryousuke' && password == '1637'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
     end
   end
 end
